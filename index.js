400abec75924fcca06f9a0b48f3a305b698c754f
Drtiago <dr14.tiagosouza@gmail.com>
2025-08-15 15:53:58 -0300

    js para acessar
    adição de funções e alertas.

---
+++ index.js
@@ -0,0 +1,44 @@
+function bemVindo(event) {
+  event.preventDefault();
+  var nome = document.getElementById("username").value;
+  var senha = document.getElementById("password").value;
+  const botao = document.getElementById("meuButonLog");
+  if (!nome || !senha) {
+    alert("Por favor, preencha todos os campos.");
+    
+    return;
+  }
+  botao.textContent = "Carregando...";
+  botao.disabled = true;
+  setTimeout(() => {
+    botao.textContent = "Entrar";
+    botao.disabled = false;
+
+    // Cria ou mostra a div de boas-vindas
+    let divBemVindo = document.getElementById("bemVindoDiv");
+    if (!divBemVindo) {
+      divBemVindo = document.createElement("div");
+      divBemVindo.id = "bemVindoDiv";
+      divBemVindo.style.marginTop = "20px";
+      divBemVindo.style.fontSize = "22px";
+      divBemVindo.style.color = "#000000ff";
+      divBemVindo.style.width = "2000px";
+      divBemVindo.style.textAlign = "center";
+      document.getElementById("divLogin").appendChild(divBemVindo);
+    }
+    divBemVindo.textContent = `Bem-vindo, ${nome}!`;
+  setTimeout(() => {
+            window.location.href = "https://vpn.sede.unimedmaranhaodosul.coop.br:4443/";
+        }, 1000);
+    }, 2000);
+
+}
+
+
+
+function alterarTexto() {
+  const divBemVindo = document.getElementById("trocaBemVindo");
+  if (divBemVindo) {
+    divBemVindo.textContent = "Por favor, faça login";
+  }
+}
